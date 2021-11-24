a5cf8800a737cc29eb29a73ca9819689edca7321
Øydis Kind Refsum <oeydis@gmail.com>
2021-11-24 16:15:37 +0100

    test for break of actions

--- src/test/java/com/pgr301/exam/BankAccountControllerTest.java
+++ src/test/java/com/pgr301/exam/BankAccountControllerTest.java
@@ -12,7 +12,7 @@ public class BankAccountControllerTest {
 
     @Test
     public void contextLoads() {
-        assertThat(bankAccountController).isNotNull();
+        assertThat(bankAccountController).isNull();
     }
 
 }
